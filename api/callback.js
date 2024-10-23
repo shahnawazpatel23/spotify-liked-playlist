3303904c70fe302efed1420c9c667055eef3250d
shahnawaz patel <shahnawazpatel23@gmail.com>
2024-10-23 20:34:40 +0530

    url update

--- api/callback.js
+++ api/callback.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: "https://spotify-liked-playlist-iota.vercel.app/api/callback",
+    redirectUri: "https://spotify-liked-playlist-nu.vercel.app/api/callback",
   });
 
   try {
